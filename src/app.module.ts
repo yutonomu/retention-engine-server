2b8fa1214bd23581eeb86721859c9ab882c67d84
ゆうと <yutonomun@gmail.com>
2025-11-20 03:04:06 +0900

    feat: グローバル設定のためにConfigModuleを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LlmModule } from './llm/llm.module';
 
 @Module({
-  imports: [LlmModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), LlmModule],
   controllers: [AppController],
   providers: [AppService],
 })
