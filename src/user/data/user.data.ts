15bf7f0c338fd786bb1c799a1877427e04ba918e
ゆうと <yutonomun@gmail.com>
2025-12-02 22:27:11 +0900

    ユーざーオブジェクトを使用するように

--- src/user/data/user.data.ts
+++ src/user/data/user.data.ts
@@ -1,39 +1,44 @@
 import { User } from '../user.types';
 
 export const userData: User[] = [
-  {
+  User.create({
     user_id: 'user-001',
     role: 'NEW_HIRE',
     display_name: '佐藤 太郎',
     email: 'taro.sato@example.com',
     created_at: new Date('2024-12-15T05:00:00Z'),
-  },
-  {
+    mbti: null,
+  }),
+  User.create({
     user_id: 'user-002',
     role: 'MENTOR',
     display_name: '山田 花子',
     email: 'hanako.yamada@example.com',
     created_at: new Date('2024-12-10T03:20:00Z'),
-  },
-  {
+    mbti: null,
+  }),
+  User.create({
     user_id: 'user-003',
     role: 'NEW_HIRE',
     display_name: '鈴木 次郎',
     email: 'jiro.suzuki@example.com',
     created_at: new Date('2024-12-18T01:15:00Z'),
-  },
-  {
+    mbti: null,
+  }),
+  User.create({
     user_id: 'user-004',
     role: 'MENTOR',
     display_name: '佐々木 由美',
     email: 'yumi.sasaki@example.com',
     created_at: new Date('2024-11-30T02:05:00Z'),
-  },
-  {
+    mbti: null,
+  }),
+  User.create({
     user_id: 'user-005',
     role: 'NEW_HIRE',
     display_name: '田中 蓮',
     email: 'ren.tanaka@example.com',
     created_at: new Date('2024-12-20T08:45:00Z'),
-  },
+    mbti: null,
+  }),
 ];
